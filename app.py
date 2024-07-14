2d49522a40cb7d95d43835b3cf7f44d1a62489e3
donowner <150280482+donowner@users.noreply.github.com>
2024-07-14 18:42:12 +0530

    Update app.py

--- app.py
+++ app.py
@@ -37,10 +37,6 @@ async def root_route_handler(_):
         }
     )
     
-async def web_server():
-    web_app = web.Application(client_max_size=30000000)
-    web_app.add_routes(routes)
-    return web_app
 
 
 # Function to download the video thumbnail
